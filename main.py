37b0f378ea8f651fef6bcd2e4c79a29f6870c232
Geo99pro <lolo@aluno.puc-rio.br>
2025-05-19 20:52:40 -0300

    refactor: Organize imports and update logger configuration in main.py for clarity and consistency

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import time
-
-import comet_ml
-import hydra
-import lightning as pl
 import torch
-from dataset import EarthViewNEONDatamodule
-from lightning.pytorch.callbacks import (
-    EarlyStopping,
-    LearningRateMonitor,
-    ModelCheckpoint,
-)
+import hydra
+import comet_ml
+import lightning as pl
+
+from omegaconf import DictConfig
+from dataset import PrepareDataset
 from lightning.pytorch.loggers import CometLogger
 from lightning_model import DepthAnythingV2Module
-from omegaconf import DictConfig
+from lightning.pytorch.callbacks import (EarlyStopping, LearningRateMonitor, ModelCheckpoint)
 
 
 @hydra.main(config_path="configs", config_name="default", version_base=None)
 def main(args: DictConfig):
     pl.seed_everything(42)
     torch.set_float32_matmul_precision("medium")
+    
+    custom_dataset = PrepareDataset(**args.dataset)
+    train_dataset, val_dataset = custom_dataset.get_train_val_loaders()
 
-    data_module = EarthViewNEONDatamodule(**args.dataset)
+    #data_module = EarthViewNEONDatamodule(**args.dataset)
     model = DepthAnythingV2Module(**args.model)
 
     experiment_id = time.strftime("%Y%m%d-%H%M%S")
     logger = False
     if args.logger:
         logger = CometLogger(
-            project_name="depth-any-canopy",
-            workspace="",
-            experiment_name="",
-            save_dir="comet-logs",
-            offline=False,
+            api_key="9gB1lNby6NfLqBASYqyTh7oaD",
+            workspace="depth-any-canopy-test-1",
+            project="depth-any-canopy",
+            experiment_name=f"depth-any-canopy-{experiment_id}",
+            mode="create",
+            online=True,
         )
         experiment_id = logger.experiment.id
 
@@ -60,10 +60,12 @@ def main(args: DictConfig):
         log_every_n_steps=50,
         precision="32-true" if args.model.encoder == "vitl" else "32-true",
         limit_val_batches=50,
-        val_check_interval=500,
+        val_check_interval=1.0,
     )
 
-    trainer.fit(model, datamodule=data_module)
+    trainer.fit(model, 
+                train_dataloaders=train_dataset, 
+                val_dataloaders=val_dataset)
 
 
 if __name__ == "__main__":
