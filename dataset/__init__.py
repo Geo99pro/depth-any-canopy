fdd1de80759e2b1228a057b3b2b1f8facf2160ff
Geo99pro <lolo@aluno.puc-rio.br>
2025-08-28 17:01:06 -0300

    fix: 🐛 Correct import statements for PrepareDataset and ProcessData modules in __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,6 @@
 from .earthview_neon import EarthViewNEON, EarthViewNEONDatamodule
 from .hres_canopy import HResCanopyDatamodule
-from .custom_dataset import PrepareDataset
+from .prepare_dataset import PrepareDataset
+from .prepare_dataset_full_data import PrepareDatasetFullData
+#from .process_data import ProcessData
+from .process_data_clean import ProcessData
