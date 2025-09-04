d250c908a91d22df3c33ca16c7b2e6d4de96d404
Geo99pro <lolo@aluno.puc-rio.br>
2025-09-04 17:14:32 -0300

    refactor: 🔄 Comment out unused imports for EarthViewNEON and HResCanopyDatamodule in __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,5 +1,5 @@
-from .earthview_neon import EarthViewNEON, EarthViewNEONDatamodule
-from .hres_canopy import HResCanopyDatamodule
+#from .earthview_neon import EarthViewNEON, EarthViewNEONDatamodule
+#from .hres_canopy import HResCanopyDatamodule
 from .prepare_dataset import PrepareDataset
 from .prepare_dataset_full_data import PrepareDatasetFullData
 #from .process_data import ProcessData
